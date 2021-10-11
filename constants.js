4fac113517ea9d0e358f1796d9bfd447422a6b66
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-10-11 17:36:53 +0200

    CometChat SDK installed and configured API Keys

---
+++ constants.js
@@ -0,0 +1,5 @@
+export const COMETCHAT_CONSTANTS = {
+  APP_ID: '1945928386570244',
+  REGION: 'eu',
+  AUTH_KEY: 'efc7ec1b79ac719f16161c4bb8f9462d269e2e39',
+};
--- index.js
+++ index.js
@@ -5,5 +5,22 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {CometChat} from '@cometchat-pro/react-native-chat';
+import {COMETCHAT_CONSTANTS} from './constants';
+
+const appSetting = new CometChat.AppSettingsBuilder()
+  .subscribePresenceForAllUsers()
+  .setRegion(COMETCHAT_CONSTANTS.REGION)
+  .build();
+CometChat.init(COMETCHAT_CONSTANTS.APP_ID, appSetting).then(
+  () => {
+    console.warn('Initialization completed successfully');
+    // You can now call warnin function.
+  },
+  error => {
+    console.log('Initialization failed with error:', error);
+    // Check the reason for error and take appropriate action.
+  },
+);
 
 AppRegistry.registerComponent(appName, () => App);
